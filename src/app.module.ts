914cee24444163941357097bbe1c2d8b6f0fb9a0
Arvind-4 <lordarvindthegamer@gmail.com>
2023-04-08 16:40:53 +0530

    Added: home route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from '@src/user/user.module';
 import { ConfigModule } from '@src/config/config.module';
+import { HomeModule } from '@src/home/home.module';
 
 @Module({
-  imports: [UserModule, ConfigModule],
+  imports: [UserModule, ConfigModule, HomeModule],
 })
 export class AppModule {}
