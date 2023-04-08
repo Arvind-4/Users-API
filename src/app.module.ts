aa2daf8eb7fcab562b306cce58a1d2218a6d916c
Arvind-4 <lordarvindthegamer@gmail.com>
2023-04-08 15:51:33 +0530

    Updated: main files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from '@src/app.controller';
-import { AppService } from '@src/app.service';
 import { UserModule } from '@src/user/user.module';
 import { ConfigModule } from '@src/config/config.module';
 
 @Module({
   imports: [UserModule, ConfigModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@src/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 import { ConfigModule } from '@src/config/config.module';
 
@@ -7,6 +8,8 @@ const port = ConfigModule.port || 8000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
+  console.log(`🚀 Server running on http://localhost:${port}`);
 }
 bootstrap();
